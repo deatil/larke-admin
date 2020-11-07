91fb2b4cac892fd16a113ca0eaf1e3bc2ae64b61
deatil <2217957370@qq.com>
2020-11-07 13:36:07 +0800

    优化接口地址

--- src/larke/resource/routes/admin.php
+++ src/larke/resource/routes/admin.php
@@ -17,9 +17,9 @@ Route::group([
         $router->get('/auth/rule/{id}', 'AuthRule@detail')->name('auth-rule-detail');
         $router->post('/auth/rule/create', 'AuthRule@create')->name('auth-rule-create');
         $router->put('/auth/rule/{id}', 'AuthRule@update')->name('auth-rule-update');
-        $router->patch('/auth/rule/listorder/{id}', 'AuthRule@listorder')->name('auth-rule-listorder');
-        $router->patch('/auth/rule/enable/{id}', 'AuthRule@enable')->name('auth-rule-enable');
-        $router->patch('/auth/rule/disable/{id}', 'AuthRule@disable')->name('auth-rule-disable');
+        $router->patch('/auth/rule/{id}/sort', 'AuthRule@listorder')->name('auth-rule-listorder');
+        $router->patch('/auth/rule/{id}/enable', 'AuthRule@enable')->name('auth-rule-enable');
+        $router->patch('/auth/rule/{id}/disable', 'AuthRule@disable')->name('auth-rule-disable');
         $router->delete('/auth/rule/{id}', 'AuthRule@delete')->name('auth-rule-delete');
         
         $router->get('/auth/group/index', 'AuthGroup@index')->name('auth-group-index');
@@ -28,11 +28,11 @@ Route::group([
         $router->get('/auth/group/{id}', 'AuthGroup@detail')->name('auth-group-detail');
         $router->post('/auth/group/create', 'AuthGroup@create')->name('auth-group-create');
         $router->put('/auth/group/{id}', 'AuthGroup@update')->name('auth-group-update');
-        $router->patch('/auth/group/listorder/{id}', 'AuthGroup@listorder')->name('auth-group-listorder');
-        $router->patch('/auth/group/enable/{id}', 'AuthGroup@enable')->name('auth-group-enable');
-        $router->patch('/auth/group/disable/{id}', 'AuthGroup@disable')->name('auth-group-disable');
+        $router->patch('/auth/group/{id}/sort', 'AuthGroup@listorder')->name('auth-group-listorder');
+        $router->patch('/auth/group/{id}/enable', 'AuthGroup@enable')->name('auth-group-enable');
+        $router->patch('/auth/group/{id}/disable', 'AuthGroup@disable')->name('auth-group-disable');
         $router->delete('/auth/group/{id}', 'AuthGroup@delete')->name('auth-group-delete');
-        $router->put('/auth/group/access/{id}', 'AuthGroup@access')->name('auth-group-access');
+        $router->put('/auth/group/{id}/access', 'AuthGroup@access')->name('auth-group-access');
     });
     
     $router->get('/passport/captcha', 'Passport@captcha')->name('passport-captcha');
@@ -47,8 +47,8 @@ Route::group([
     
     $router->get('/attachment/index', 'Attachment@index')->name('attachment-index');
     $router->get('/attachment/{id}', 'Attachment@detail')->name('attachment-detail');
-    $router->patch('/attachment/enable/{id}', 'Attachment@enable')->name('attachment-enable');
-    $router->patch('/attachment/disable/{id}', 'Attachment@disable')->name('attachment-disable');
+    $router->patch('/attachment/{id}/enable', 'Attachment@enable')->name('attachment-enable');
+    $router->patch('/attachment/{id}/disable', 'Attachment@disable')->name('attachment-disable');
     $router->delete('/attachment/{id}', 'Attachment@delete')->name('attachment-delete');
     $router->post('/attachment/upload', 'Attachment@upload')->name('attachment-upload');
     $router->get('/attachment/downcode/{id}', 'Attachment@downloadCode')->name('attachment-download-code');
@@ -59,20 +59,20 @@ Route::group([
     $router->get('/admin/rules/{id}', 'Admin@rules')->name('admin-rules');
     $router->post('/admin/create', 'Admin@create')->name('admin-create');
     $router->put('/admin/{id}', 'Admin@update')->name('admin-update');
-    $router->patch('/admin/enable/{id}', 'Admin@enable')->name('admin-enable');
-    $router->patch('/admin/disable/{id}', 'Admin@disable')->name('admin-disable');
+    $router->patch('/admin/{id}/enable', 'Admin@enable')->name('admin-enable');
+    $router->patch('/admin/{id}/disable', 'Admin@disable')->name('admin-disable');
     $router->delete('/admin/{id}', 'Admin@delete')->name('admin-delete');
-    $router->put('/admin/password/{id}', 'Admin@changePasssword')->name('admin-password');
-    $router->put('/admin/access/{id}', 'Admin@access')->name('admin-access');
+    $router->put('/admin/{id}/password', 'Admin@changePasssword')->name('admin-password');
+    $router->put('/admin/{id}/access', 'Admin@access')->name('admin-access');
     $router->post('/admin/logout/{refreshToken}', 'Admin@logout')->name('admin-logout');
     
     $router->get('/config/index', 'Config@index')->name('config-index');
     $router->get('/config/{id}', 'Config@detail')->name('config-detail');
     $router->post('/config/create', 'Config@create')->name('config-create');
     $router->put('/config/{id}', 'Config@update')->name('config-update');
-    $router->patch('/config/listorder/{id}', 'Config@listorder')->name('config-listorder');
-    $router->patch('/config/enable/{id}', 'Config@enable')->name('config-enable');
-    $router->patch('/config/disable/{id}', 'Config@disable')->name('config-disable');
+    $router->patch('/config/{id}/sort', 'Config@listorder')->name('config-listorder');
+    $router->patch('/config/{id}/enable', 'Config@enable')->name('config-enable');
+    $router->patch('/config/{id}/disable', 'Config@disable')->name('config-disable');
     $router->delete('/config/{id}', 'Config@delete')->name('config-delete');
     $router->put('/config/setting', 'Config@setting')->name('config-setting');
     
@@ -82,13 +82,13 @@ Route::group([
     
     $router->get('/extension/index', 'Extension@index')->name('extension-index');
     $router->get('/extension/local', 'Extension@local')->name('extension-local');
-    $router->put('/extension/config/{name}', 'Extension@config')->name('extension-config');
     $router->post('/extension/install/{name}', 'Extension@install')->name('extension-install');
     $router->delete('/extension/uninstall/{name}', 'Extension@uninstall')->name('extension-uninstall');
     $router->put('/extension/upgrade/{name}', 'Extension@upgrade')->name('extension-upgrade');
-    $router->patch('/extension/enable/{name}', 'Extension@enable')->name('extension-enable');
-    $router->patch('/extension/disable/{name}', 'Extension@disable')->name('extension-disable');
-    $router->patch('/extension/listorder/{name}', 'Extension@listorder')->name('extension-listorder');
+    $router->put('/extension/{name}/config', 'Extension@config')->name('extension-config');
+    $router->patch('/extension/{name}/enable', 'Extension@enable')->name('extension-enable');
+    $router->patch('/extension/{name}/disable', 'Extension@disable')->name('extension-disable');
+    $router->patch('/extension/{name}/sort', 'Extension@listorder')->name('extension-listorder');
     $router->post('/extension/upload', 'Extension@upload')->name('extension-upload');
     
     $router->get('/system/info', 'System@info')->name('system-info');
