c92bc882df11b2ca4dab7e0b4a6b12c3e7717cf0
deatil <2217957370@qq.com>
2020-12-15 00:05:53 +0800

    添加扩展配置获取

--- src/larke/helper.php
+++ src/larke/helper.php
@@ -1,9 +1,10 @@
 <?php
 
+use Larke\Admin\Model\Extension as ExtensionModel;
 use Larke\Admin\Traits\ResponseJson as ResponseJsonTrait;
 
 if (!function_exists('larke_success')) {
-    /*
+    /**
      * 返回成功JSON
      *
      * @create 2020-10-19
@@ -22,7 +23,7 @@ if (!function_exists('larke_success')) {
 }
 
 if (!function_exists('larke_error')) {
-    /*
+    /**
      * 返回失败JSON
      *
      * @create 2020-10-19
@@ -37,5 +38,19 @@ if (!function_exists('larke_error')) {
                 return $this->error($message, $code, $data, $header);
             }
         })->json($message, $code, $data, $header);
+    }
+}
+
+if (!function_exists('larke_extension_config')) {
+    /**
+     * 扩展配置信息
+     *
+     * @create 2020-12-15
+     * @author deatil
+     */
+    function larke_extension_config($name) {
+        return ExtensionModel::where('name', '=', $name)
+            ->first()
+            ->config_datas;
     }
 }
