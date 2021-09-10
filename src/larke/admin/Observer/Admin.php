7c25f835f46dd4a7a909036b4e4844a9366cbc92
deatil <2217957370@qq.com>
2021-09-10 23:00:47 +0800

    添加管理员账号删除同时删除关联用户组关系

--- src/larke/admin/Observer/Admin.php
+++ src/larke/admin/Observer/Admin.php
@@ -5,9 +5,13 @@ declare (strict_types = 1);
 namespace Larke\Admin\Observer;
 
 use Larke\Admin\Model\Admin as AdminModel;
+use Larke\Admin\Model\AuthGroupAccess as AuthGroupAccessModel;
 
 class Admin
 {
+    /**
+     * 创建前
+     */
     public function creating(AdminModel $model)
     {
         $model->id = md5(mt_rand(100000, 999999).microtime());
@@ -18,4 +22,17 @@ class Admin
         $model->create_time = time();
         $model->create_ip = request()->ip();
     }
+    
+    /**
+     * 删除后
+     */
+    public function deleted(AdminModel $model)
+    {
+        AuthGroupAccessModel
+            ::where('admin_id', $model->id)
+            ->get()
+            ->each
+            ->delete();
+    }
+
 }
