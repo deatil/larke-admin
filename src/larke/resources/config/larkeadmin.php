0834e0323a5a7d2c2683b206a98d72ffee3f500d
deatil <2217957370@qq.com>
2023-02-12 00:18:06 +0800

    更新

--- src/larke/resources/config/larkeadmin.php
+++ src/larke/resources/config/larkeadmin.php
@@ -74,7 +74,7 @@ return [
             'secrect'   => env('LARKE_ADMIN_JWT_SIGNER_SECRECT', 'NzdjMTNmODU0YzA5ODg5ZjJiY2UzYWY4OTUwOWFkMzY='),
             
             // 非对称加密使用
-            // 示例: storage_path('key/rsa/rsa-pkcs8')
+            // 示例: storage_path('key/rsa/rsa-pkcs8') | MTIz
             // EdDSA key 请填写 base64 编码的字符，其他的填写 key 路径即可
             // RS256, RS384, RS512
             // ES256, ES384, ES512
